d50172b8046a2fab1ec1c35609d16b1ee071bfa5
mohammadhastamtehrandotcomnbg2017 <mohammadhastamtehrandotcomnbg2017@users.noreply.github.com>
2017-03-03 15:40:50 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
-bot_token = "178560551:AAGZtcrVedRN64xUx08dCM7cuC4mtldT2h0"
+bot_token = "248333195:AAHVaFKVihv0jrA2OHR9FqC3J_ZbfFkcNCE"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.7"
-sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 180910633
-sudo_user = "shayan_soft"
+sudo_name = "Mizbanivps.com"
+sudo_id = 250049437
+sudo_user = "Mizbanivps.com"
 nerkh = "5"
